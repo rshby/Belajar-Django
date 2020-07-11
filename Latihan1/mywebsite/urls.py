0d3f855abd62804c7d555335eb8b36f459c4686b
Reo Sahobby <reoshby@gmail.com>
2020-07-11 13:50:13 +0700

    Update app

--- Latihan1/mywebsite/urls.py
+++ Latihan1/mywebsite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from blog import views as viewsBlog
+from about import views as viewsAbout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^ ', views.index),
+    url(r'^$', views.index),
+    url(r'^blog/$', viewsBlog.index), #=> mengakses link blog
+    url(r'^about/$', viewsAbout.index), #=> mengakses link about
 ]
