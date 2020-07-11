eea5a3ad404c3d7017bd0747f7e0ced83be2d867
Reo Sahobby <reoshby@gmail.com>
2020-07-11 14:59:44 +0700

    Update

--- Latihan1/blog/views.py
+++ Latihan1/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #=> index untuk si blog
-    return render(request, 'blog.html')
+    return render(request, 'blog/blog.html')
 
 def recent(request):
     return HttpResponse('<h1>ini adalah recent post</h1>')
