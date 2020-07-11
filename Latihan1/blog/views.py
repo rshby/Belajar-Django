4ebcac959fe672f22db504bebcd7fd55e08feb6e
Reo Sahobby <reoshby@gmail.com>
2020-07-11 14:19:37 +0700

    Update URL pembenaran

--- Latihan1/blog/views.py
+++ Latihan1/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #=> index untuk si blog
     return render(request, 'blog.html')
+
+def recent(request):
+    return HttpResponse('<h1>ini adalah recent post</h1>')
--- Latihan1/mywebsite/urls.py
+++ Latihan1/mywebsite/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
-from blog import views as viewsBlog
-from about import views as viewsAbout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
-    url(r'^blog/$', viewsBlog.index), #=> mengakses link blog
-    url(r'^about/$', viewsAbout.index), #=> mengakses link about
+    url(r'^blog/', include('blog.urls')), #=> mengakses link blog
+    url(r'^about/', include('about.urls')), #=> mengakses link about
 ]
