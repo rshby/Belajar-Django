ee052df70b6279ff85946d1abc1b7a3e9eee5628
Reo Sahobby <reoshby@gmail.com>
2020-07-11 19:27:40 +0700

    Update Context

--- Latihan1/blog/urls.py
+++ Latihan1/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^profile/$', views.profile),
     url(r'^$', views.index),
     url(r'^recent/$', views.recent),
 ]
--- Latihan1/blog/views.py
+++ Latihan1/blog/views.py
@@ -1,9 +1,28 @@
+
+##==== Views Blog ===
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #=> index untuk si blog
-    return render(request, 'blog/blog.html')
+    context = {
+        "judul" : "Halaman Blog Website",
+        "coder" : "Reo Sahobby"
+    }
+    return render(request, 'blog/index.html', context)
 
 def recent(request):
-    return HttpResponse('<h1>ini adalah recent post</h1>')
+    context = {
+        "judul" : "Halaman Recent Apps",
+        "coder" : "Reo S",
+    }
+    return render(request, 'blog/index.html', context)
+
+def profile(request):
+    context = {
+        "judul" : "Halaman Profil Penulis",
+        "coder" : "Reo Sahobby",
+        "alamat" : "Klaten"
+    }
+    return render(request, 'blog/index.html', context)
